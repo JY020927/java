5a91cf7af6a6ab2302b696b28dddfd7bad5b3f1d
JY020927 <139797998+JY020927@users.noreply.github.com>
2023-07-21 17:14:34 +0900

    Delete Downloads/JY_2103361/JY_2103361 directory

--- Downloads/JY_2103361/JY_2103361/Test01_2103361/src/Test.java
+++
@@ -1,8 +0,0 @@
-
-public class Test {
-	public static void main(String[] ar) {
-		System.out.println("Hi");
-		System.out.println("실행 단축키 : ctrl+F11");
-	}
-
-}
--- Downloads/JY_2103361/JY_2103361/Test02_2103361/src/abc/Test.java
+++
@@ -1,8 +0,0 @@
-package abc;
-
-public class Test {
-	public static void main(String[] ar) {
-		System.out.println("¾È³ç");
-	}
-
-}
